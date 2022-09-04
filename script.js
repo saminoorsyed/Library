82276d8f4d7e59708e32c896941fee38497350b7
saminoorsyed <saminoorsyed@gmail.com>
2022-09-04 17:13:57 -0400

    Added logic for individual cards
    functionality includes reading input from a form, making html elements
    and adding them to the card
    
    also included a remove button that removes the div containing the button
    clicked

--- script.js
+++ script.js
@@ -1,20 +1,90 @@
-let myLibrary = [];
+const addPlaylist = document.querySelector('add-playlist');
+const playlist = document.querySelector('.playlist-form')
 
-function Book(title, author, pages, read){
+const addSong = document.querySelectorAll('.add-song');
+const form = document.querySelector('form');
+const titleIn = document.getElementById ('title');
+const artistIn = document.getElementById ('artist');
+const albumIn = document.getElementById ('album');
+const urlIn = document.getElementById ('url');
+const songs = document.querySelector('.songs');
+
+function moveAskTitle(){
+    
+}
+function Song (title, artist, album, url) {
     this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = false
-    this.info = function (){
-        return `${title} by ${author}, ${pages}pages, ${(read)?"finished":"not read yet"}`;
-    }
+    this.artist = artist;
+    this.album = album;
+    this.url = url 
 }
 
-Book.prototype.readIt = function () {this.read = true}
-const sami = new Book( 'sami','Sami', 'too many', false);
+const songDivMaker = function(title, artist, album, url) {
+    // declare newSong with inputs, a dive to place a link inside and the link + a button to remove that song
+    const newSong = new Song(title, artist, album, url);
+    const songDiv = document.createElement('div');
+    const a = document.createElement('a');
+    const removeBut = document.createElement('button');
+    const songClass = newSong.title.replace(/\s/g,'-');
+    songDiv.classList.add(`${songClass}`);
+    songDiv.dataset.name = newSong.title;
+    a.href = newSong.url;
+    // populate the text of the link to include the info from the song
+    a.textContent = `${newSong.title} by ${newSong.artist} on the album ${newSong.album}`;
+    removeBut.textContent = 'remove song';
+    removeBut.addEventListener('click', removeSong);
+    songs.appendChild(songDiv);
+    songDiv.appendChild(a);
+    songDiv.appendChild(removeBut);
+    
+}
 
-console.log(sami.read);
+const clear = function() {
 
-sami.readIt();
+} 
 
-console.log(sami.read);
+const removeSong = function(e){
+    e.target.parentNode.innerHTML = '';
+    // const songToRemove = document.querySelector(`${title}`);
+    // songToRemove.innerHTML = '';
+}
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    songDivMaker(titleIn.value, artistIn.value, albumIn.value, urlIn.value);
+});
+
+addPlaylist.addEventListener('click', askTitle)
+
+
+
+
+
+
+
+
+
+
+
+
+// let myLibrary = [];
+
+// function Song(title, artist, time, album){
+//     this.title = title;
+//     this.artist = artist;
+//     this.time = time;
+//     this.album = false
+//     this.info = function (){
+//         return `${title} by ${artist}, ${time}time, ${(album)?"finished":"not album yet"}`;
+//     }
+// }
+
+// Book.prototype.readIt = function () {this.album = true}
+// const sami = new Book( 'sami','Sami', 'too many', false);
+
+// console.log(sami.album);
+
+// sami.readIt();
+
+// console.log(sami.album);
